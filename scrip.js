b26bb6c127e16a447eb6bbe25639e533aae1448e
LytzyMorales <lytzy.morales23@gmail.com>
2024-05-30 23:49:30 -0600

    nuevo

--- scrip.js
+++ scrip.js
@@ -4,6 +4,10 @@ function mostrarSabores() {
     let nombreSabor = document.querySelector("#sabor")
     nombreSabor.innerHTML = sabores[Math.floor(Math.random() * sabores.length)]
 
+    const saludos = ["Lytzy", "Adamari", "ada"]
+    let saludo = document.querySelector("#saludo")
+    saludo.innerHTML = saludos[Math.floor(Math.random() * saludos.length)]
+
     console.log("Helado")
 }
 
